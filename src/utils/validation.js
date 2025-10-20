b0d742fbbb840e4b329f5bc9c6baee3c2784382f
Enoti4Ka <Enoti4Ka44@gmail.com>
2025-10-21 01:48:21 +0300

    feat(auth): add validation of the form

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,3 +1,48 @@
-export function validateAuth({username, email, password, confirmPassword}) {
-    if 
+export function validateUsername(username) {
+    if (!username) {
+        return "Username is necessary"
+    }
+
+    if (username.length < 3) {
+        return "Username must be at least 3 characters"
+    }
+
+    return null
+}
+
+export function validateEmail(email) {
+    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!email) {
+        return "Email is necessary"
+    }
+
+    if (!pattern.test(email)) {
+      return "Invalid email";
+    }
+
+    return null
+
+}
+
+export function validatePassword(password) {
+    if (!password) {
+        return "Password is necessary"
+    }
+    if (password.length < 8) {
+        return "Passwords must be at least 8 characters";
+    }
+
+    return null
+}
+
+export function validateConfirmPassword(confirmPassword) {
+    if (!confirmPassword) {
+        return "Please confirm your password"
+    }
+
+    if (confirmPassword !== password) {
+        return "Passwords must match"
+    }
+
+    return null
 }
